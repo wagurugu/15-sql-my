a12ff5f5a7c058b68030901484a874ef70ba179e
wagurugu <cutygost@nate.com>
2021-10-18 17:06:17 +0900

    15.sql 최종완성

--- api/routes/book/index.js
+++ api/routes/book/index.js
@@ -5,8 +5,8 @@ const listRouter = require('./list-router')
 const viewRouter = require('./view-router')
 const downloadRouter = require('./download-router')
 
+router.use('/', listRouter)											// HTML/GET: 리스트페이지
 router.use('/view', viewRouter)									// HTML/GET: 상세페이지
 router.use('/download', downloadRouter)					// HTML/GET: 상세페이지
-router.use('/', listRouter)											// HTML/GET: 리스트페이지
 
 module.exports = router
