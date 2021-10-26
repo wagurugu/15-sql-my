f59620332ddaed991c8a2e00a2033f22ad2bf7cb
wagurugu <cutygost@nate.com>
2021-10-26 12:21:16 +0900

    1111

--- api/app.js
+++ api/app.js
@@ -1,36 +1,35 @@
 /************* global require *************/
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const cors = require("cors");
 
 /*************** server init **************/
-require('./modules/server-init')(app, process.env.PORT)
+require("./modules/server-init")(app, process.env.PORT);
 
+/*************** helmet init **************/
+app.use(helmet({ contentSecurityPolicy: false }));
 
 /*************** middleware ***************/
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors({ origin: true, credentials: true }))
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: true, credentials: true }));
+app.use(cookieParser());
 
 /*************** logger init **************/
-const logger = require('./middlewares/morgan-mw')
-app.use(logger)
-
+const logger = require("./middlewares/morgan-mw");
+app.use(logger);
 
 /*************** router init **************/
-const bookRouter = require('./routes/book')
+const bookRouter = require("./routes/book");
 
-app.use('/book', bookRouter)
-
+app.use("/book", bookRouter);
 
 /**************** error init **************/
-const _404Router = require('./routes/error/404-router')
-const _500Router = require('./routes/error/500-router')
+const _404Router = require("./routes/error/404-router");
+const _500Router = require("./routes/error/500-router");
 
-app.use(_404Router)
-app.use(_500Router)
-
+app.use(_404Router);
+app.use(_500Router);
--- back/app.js
+++ back/app.js
@@ -1,75 +1,69 @@
 /************* global require *************/
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const path = require('path')
-const passport = require('passport')
-const passportModule = require('./passport')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const helmet = require("helmet");
+const passport = require("passport");
+const passportModule = require("./passport");
 
-const method = require('./middlewares/method-mw')
-const logger = require('./middlewares/morgan-mw')
-const session = require('./middlewares/session-mw')
-const locals = require('./middlewares/locals-mw')
-const langMW = require('./middlewares/lang-mw')
-
+const method = require("./middlewares/method-mw");
+const logger = require("./middlewares/morgan-mw");
+const session = require("./middlewares/session-mw");
+const locals = require("./middlewares/locals-mw");
+const langMW = require("./middlewares/lang-mw");
 
 /*************** server init **************/
-require('./modules/server-init')(app, process.env.PORT)
+require("./modules/server-init")(app, process.env.PORT);
 
+/*************** helmet init **************/
+app.use(helmet({ contentSecurityPolicy: false }));
 
 /*************** static init **************/
-app.use('/', express.static(path.join(__dirname, 'public')))
-app.use('/uploads', express.static(path.join(__dirname, 'storages')))
-
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "storages")));
 
 /************** view engine ***************/
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.locals.pretty = true
-
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.locals.pretty = true;
 
 /*************** middleware ***************/
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(method())
-app.use(session(app))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(method());
+app.use(session(app));
 
 /**************** passport ****************/
-passportModule(passport)
-app.use(passport.initialize())
-app.use(passport.session())
-
+passportModule(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 /***************** locals *****************/
-app.use(locals)
-
+app.use(locals);
 
 /*************** logger init **************/
-app.use(logger)
-
+app.use(logger);
 
 /*************** router init **************/
-const bookRouter = require('./routes/book')
-const apiBookRouter = require('./routes/api/book')
-const authRouter = require('./routes/auth')
-const apiAuthRouter = require('./routes/api/auth')
-const mypageRouter = require('./routes/mypage')
-const apiMypageRouter = require('./routes/api/mypage')
+const bookRouter = require("./routes/book");
+const apiBookRouter = require("./routes/api/book");
+const authRouter = require("./routes/auth");
+const apiAuthRouter = require("./routes/api/auth");
+const mypageRouter = require("./routes/mypage");
+const apiMypageRouter = require("./routes/api/mypage");
 
-app.use(langMW)
-app.use('/book', bookRouter)
-app.use('/api/book', apiBookRouter)
-app.use('/auth', authRouter)
-app.use('/api/auth', apiAuthRouter)
-app.use('/mypage', mypageRouter)
-app.use('/api/mypage', apiMypageRouter)
-
+app.use(langMW);
+app.use("/book", bookRouter);
+app.use("/api/book", apiBookRouter);
+app.use("/auth", authRouter);
+app.use("/api/auth", apiAuthRouter);
+app.use("/mypage", mypageRouter);
+app.use("/api/mypage", apiMypageRouter);
 
 /**************** error init **************/
-const _404Router = require('./routes/error/404-router')
-const _500Router = require('./routes/error/500-router')
+const _404Router = require("./routes/error/404-router");
+const _500Router = require("./routes/error/500-router");
 
-app.use(_404Router)
-app.use(_500Router)
-
+app.use(_404Router);
+app.use(_500Router);
